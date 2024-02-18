6fbf7cafd6f2dda9ef92c042f15aaab73626532a
delskull <Deluta1989@gmail.com>
2024-02-18 16:34:00 +0300

    feat: add slider

--- library/index.js
+++ library/index.js
@@ -5,3 +5,30 @@ let headerMenu = document.querySelector(".header");
 burgerToggle.addEventListener("click", () =>
   headerMenu.classList.toggle("open")
 );
+// slider
+let offset = 0;
+let circle2 = document.querySelector(".circle_2");
+let circle1 = document.querySelector(".circle_1");
+let circle3 = document.querySelector(".circle_3");
+const sliderLine = document.querySelector(".caroosel");
+// Скролл на  3 кружках
+circle2.addEventListener("click", function () {
+  offset = 475;
+  circle2.classList.toggle("circle_brow");
+
+  sliderLine.style.right = offset + "px";
+});
+
+circle3.addEventListener("click", function () {
+  offset = 950;
+  circle3.classList.toggle("circle_brow");
+
+  sliderLine.style.right = offset + "px";
+});
+
+circle1.addEventListener("click", function () {
+  offset = 0;
+  circle1.classList.toggle("circle_brow");
+  sliderLine.style.right = offset + "px";
+});
+// Скролл на  3 кружках
